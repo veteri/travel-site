74a87c7dbc9946d4a9d544b2c96bbb10cc8d3196
Dominique <dominique.narbel@gmail.com>
2018-06-07 07:11:56 +0200

    Added browser sync and split gulp file into single responsibility files

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,6 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var cssvars = require('postcss-simple-vars');
-var nested = require('postcss-nested');
-var cssImport = require('postcss-import');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
-gulp.task('default', function() {
 
-	console.log("My first gulp task");
 
-});
 
-gulp.task('html', function() {
-
-	console.log("Imagine something useful being done to your HTML here.");
-
-});
-
-gulp.task('styles', function() {
-
-	return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-		.pipe(gulp.dest('./app/temp/styles'));
-
-});
-
-gulp.task('watch', function() {
-
-	watch('./app/index.html', function() {
-		gulp.start('html');
-	});
-
-	watch('./app/assets/styles/**/*.css', function() {
-		gulp.start('styles');
-	});
-
-});
